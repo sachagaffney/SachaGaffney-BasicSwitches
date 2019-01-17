e6c1081f643ed5bc0b4a0631dcfb33082ff26902
sachagaffney <46717319+sachagaffney@users.noreply.github.com>
2019-01-17 13:42:14 +0000

    Initial commit

---
+++ Assets/Scripts/AutoSwitchTriggerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSwitchTriggerController : MonoBehaviour {
+	// Here I have a reference to the ScitchController so that I can turn on and off the switch
+	public SwitchController theSwitch;
+    public LightBulbController theLight; 
+
+   
+    void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Someone entered the switch trigger");
+
+		theSwitch.turnOn ();
+        theLight.LightOn();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		Debug.Log ("Someone left the switch trigger");
+
+		theSwitch.turnOff ();
+	}
+}
---
+++ Assets/Scripts/LightBulbController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBulbController : MonoBehaviour {
+
+    private Animator lightAnimator;
+    public SwitchController theSwitch; 
+
+    private void Awake()
+    {
+        lightAnimator = GetComponent<Animator>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+    public void LightOn ()
+    {
+        lightAnimator.SetBool("LightOn",true);
+    }
+}
